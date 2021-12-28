893cb61c94a1d6820ae9692f806bea7cda27b35e
Rony-Hasan <frrony0@gmail.com>
2021-12-28 18:54:27 +0600

    first ever commit

---
+++ class 01/js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+alert("Hello World");
+	console.log("This is copyright by Rony");
+	alert(5+10*10);
+	alert("This is about my self");
+	console.log("This is only for mine");
+	let name;
+	name = "Gazipur";
+	alert(name);
+	let rony = "Joydevpur"
+	alert(rony)
+	let city = "Rony", 
+	college = "Azimuddin college", 
+	bookName = "Comic";
+	
+	alert(city);
+	alert(college);
+	alert(bookName);
+	
+	rony = "Gazipur";
+	alert(rony);
+	
+	let message = 'Hi I\'m Rony';
+	let messagePractice = 'this is \"Rony';
+	
+	let nameOne = "Rony Hasan";
+	let nameTwo = 'Rony';
+	let nameThree = `Rony ${name}`;
---
+++ class 01/js/script2.js
@@ -0,0 +1,45 @@
+"use Strict";
+
+/*
+========================================
+let price = 250;
+let name = "Rony";
+let nameTwo = 'Rony Hasan';
+let nameThree = `Bipul ${price} name ${4+4}`;
+
+alert(nameThree);
+/*
+========================================
+//Infinity --- 25 / 0 ---- Infinity
+//NaN --- "hello" / 0 ---- NaN
+
+let division= 25 / 0;
+alert(division);
+
+let result = "hello" /0;
+alert(result);
+=================================
+let userData = "Rony";
+let result = userData / 2;
+alert(result);
+
+==========================
+let userName = "Rony";
+let  age = 24;
+let  Message = `Hello ${userName}, Your age is ${age}`
+alert(Message);
+
+
+let userName = "MR.Bipul Hasan Rony";
+let age = 25;
+let message = `hello ${userName}, Your age is ${age} new Style variable`
+alert(message);
+
+let message2 = "Hello "+userName+", Your age is "+age+" old Style variable";
+alert(message2);
+*/
+
+let message = "hello, I'm Rony";
+let Name = "Bipul\"Rony";
+let myText = 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, '
+ console.log(myText);
